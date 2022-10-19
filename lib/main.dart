5c4dc71b8f64adead208f887ba6a656507c06f8c
Your Name <aaaa@example.com>
2022-10-20 08:36:06 +0900

    認証キー変更

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://mmsvuxccinqbrfobnbls.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tc3Z1eGNjaW5xYnJmb2JuYmxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU2Mzc0NTMsImV4cCI6MTk4MTIxMzQ1M30.ExC0asv3Ie5rZ4S5-Y6U-ybohCTpZmh2Rm-c9AydLPM',
+    url: 'xxxx',
+    anonKey: 'xxxx',
   );
   
   runApp(const MyApp());
