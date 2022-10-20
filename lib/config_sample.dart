aa302737b569930f2806d16ab3bb8b34d504215f
Your Name <aaaa@example.com>
2022-10-20 09:53:42 +0900

    APIkey管理

---
+++ lib/config_sample.dart
@@ -0,0 +1,9 @@
+///APIキーなどを管理
+///Git管理は行わない
+
+// ignore_for_file: constant_identifier_names
+
+//supabase関連
+const SB_API_KEY = 'xxxxxxxx';
+const SB_ANON_KEY = 'xxxxxxxx';
+const DB_PASS = 'xxxxxxxx';
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,16 @@ import 'package:child_info/utils/constants.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:child_info/pages/splash_page.dart';
 
+import '../config.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  //Supabaseの初期化
+  //config_sample.dartをコピーしてconfig.dartを作成
   await Supabase.initialize(
-    url: 'xxxx',
-    anonKey: 'xxxx',
+    url: SB_API_KEY,
+    anonKey: SB_ANON_KEY,
   );
   
   runApp(const MyApp());
